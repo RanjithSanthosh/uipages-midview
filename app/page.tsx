5c81fe7246b94a9e298449be6efa34eeac170a8a
RanjithkumarSanthosh <22cs039@acetcbe.edu.in>
2025-09-08 18:45:07 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
-import { get } from "@/server/user";
+// import { get } from "@/server/user";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 export default async function Home() {
-  const data = await get();
+  // const data = await get();
   return (
     <div>
       <h1>Frontend</h1>
       <div>
-        {data.map((user) => (
+        {/* {data.map((user) => (
           <p key={user.id}>{user.name}</p>
-        ))}
+        ))} */}
       </div>
       <Button>
         <Link href="/DoctorPage" className="hover:underline">
